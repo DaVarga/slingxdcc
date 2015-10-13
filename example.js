303553057bc667568a04e8b202e55d651e3ef6b6
Daniel Varga <varga.daniel@gmx.de>
2015-10-14 00:17:38 +0200

    added stuff

--- example.js
+++ example.js
@@ -6,6 +6,8 @@
 
 "use strict";
 
+const winston       = require("winston");
+winston.level = "debug";
 let SlingIrc = require("./lib/SlingIrc");
 let SlingChannel = require("./lib/SlingChannel");
 
@@ -13,8 +15,13 @@ let channel = [];
 channel.push(new SlingChannel("#elitewarez"));
 channel.push(new SlingChannel("#elite-chat"));
 
-let server = new SlingIrc("irc.criten.net", channel);
+let server = new SlingIrc("irc.criten.net", undefined,channel);
 
 console.log(""+channel[0]);
 
-
+setTimeout(function(){
+    server.removeChannel(channel[1]);
+},10000)
+setTimeout(function(){
+    server.chans;
+},10000)
