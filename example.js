722ea5b8aad20ca12039f6a1b562e004b7208eba
Daniel Varga <varga.daniel@gmx.de>
2015-10-14 22:54:30 +0200

    logging works

--- example.js
+++ example.js
@@ -7,21 +7,23 @@
 "use strict";
 
 const winston       = require("winston");
-winston.level = "debug";
+winston.level = "info";
 let SlingIrc = require("./lib/SlingIrc");
 let SlingChannel = require("./lib/SlingChannel");
+let SlingDatabase = require("./lib/SlingDatabase");
 
 let channel = [];
 channel.push(new SlingChannel("#elitewarez"));
-channel.push(new SlingChannel("#elite-chat"));
+channel.push(new SlingChannel("#elite-chat",null,false));
 
 let server = new SlingIrc("irc.criten.net", undefined,channel);
+let logger = new SlingDatabase("elitewarez");
 
-console.log(""+channel[0]);
+server.onPackinfo(function (packData, channel, nick){
+    let id = packData.id;
+    let fileName = packData.fileName;
+    delete packData.id;
+    delete packData.fileName;
+    logger.addPack(id, fileName, nick, packData);
+});
 
-setTimeout(function(){
-    server.removeChannel(channel[1]);
-},10000)
-setTimeout(function(){
-    server.chans;
-},10000)
