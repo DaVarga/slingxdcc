4c45c1b8d452e22b9bfe832ff8075d9a33fc5309
Daniel Varga <varga.daniel@gmx.de>
2015-10-18 20:49:16 +0200

    Implemented Cached search with TTL

--- example.js
+++ example.js
@@ -6,60 +6,72 @@
 
 "use strict";
 
-const winston       = require("winston");
+const winston = require("winston");
 winston.level = "debug";
 let SlingIrc = require("./lib/SlingIrc");
 let SlingChannel = require("./lib/SlingChannel");
-let SlingLogger = require("./lib/SlingLogger");
+let SlingManager = require("./lib/SlingManager");
+let ds = require("./lib/SlingDatastore");
+let SlingDb = require("./lib/SlingDB");
+let async = require("async");
+let _ = require("lodash");
 
 let networks = [
     {
         name: "elitewarez",
-        host:"irc.criten.net",
-        channel:[
+        host: "irc.criten.net",
+        channel: [
             {
-                name:"#elitewarez",
-                observe:true
-            },{
-                name:"#elite-chat",
-                observe:false
+                name: "#elitewarez",
+                observe: true
+            }, {
+                name: "#elite-chat",
+                observe: false
             }
         ]
     },
     {
         name: "moviegods",
-        host:"irc.abjects.net",
-        channel:[
+        host: "irc.abjects.net",
+        channel: [
             {
-                name:"#moviegods",
-                observe:true
-            },{
-                name:"#mg-chat",
-                observe:false
+                name: "#moviegods",
+                observe: true
+            }, {
+                name: "#mg-chat",
+                observe: false
             }
         ]
     }
 ];
 
+let slingManager = new SlingManager();
 
-let servers = [];
-let loggers = [];
 
-for(let nw of networks){
-    let logger = new SlingLogger(nw.name);
-    loggers.push(logger);
-    let cnls = [];
-    for(let chan of nw.channel){
-        cnls.push(new SlingChannel(chan.name,null,chan.observe));
+ds.initalize((err)=> {
+    let db = new SlingDb();
+
+    for (let nw of networks) {
+        let cnls = [];
+        for (let chan of nw.channel) {
+            cnls.push(new SlingChannel(chan.name, null, chan.observe));
+        }
+        slingManager.addNetwork(nw.name, nw.host, undefined, cnls);
     }
-    let srv = new SlingIrc(nw.host, undefined,cnls);
-    srv.onPackinfo((packData, channel, nick) => {
-        let id = packData.id;
-        let fileName = packData.fileName;
-        delete packData.id;
-        delete packData.fileName;
-        logger.addPack(id, fileName, nick, packData);
-    });
-    servers.push(srv);
-}
 
+    setTimeout(()=> {
+        db.search({}, (err, docs, total, cacheKey)=> {
+            winston.debug("search", err, docs, total, cacheKey);
+
+            setTimeout(()=> {
+                let x = db.getPage(0, cacheKey);
+                winston.debug("get page", x);
+            }, 1000);
+            setTimeout(()=> {
+                let x = db.getPage(0, cacheKey);
+                winston.debug("get page", x);
+            }, 7000);
+
+        });
+    }, 10);
+});
